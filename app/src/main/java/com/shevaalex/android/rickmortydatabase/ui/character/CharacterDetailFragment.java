2ddf902906adcc388dd40f70486a272a860227f8
AlexMason <shevaalex@yahoo.co.uk>
2020-05-04 15:42:26 +0100

    CharacterDetail = episode list items are now clickable

--- app/src/main/java/com/shevaalex/android/rickmortydatabase/ui/character/CharacterDetailFragment.java
+++ app/src/main/java/com/shevaalex/android/rickmortydatabase/ui/character/CharacterDetailFragment.java
@@ -11,7 +11,6 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -80,11 +79,9 @@ public class CharacterDetailFragment extends Fragment implements View.OnClickLis
         //get recyclerview Adapter and set data to it using ViewModel
         adapter = new EpisodeAuxAdapter(this);
         binding.recyclerviewCharacterDetail.setAdapter(adapter);
-        Log.d("onCreateView:", "clicked char ID " + characterId);
         viewModel.getEpisodeList(characterId).observe(getViewLifecycleOwner(), episodes -> {
             adapter.setEpisodeList(episodes);
             episodeList = episodes;
-            Log.d("viewModel.getEpisodeLis", episodes.toString());
         });
         return view;
     }
@@ -150,6 +147,13 @@ public class CharacterDetailFragment extends Fragment implements View.OnClickLis
 
     @Override
     public void onEpisodeClick(int position, View v) {
-
+            Episode clickedEpisode = episodeList.get(position);
+            if (clickedEpisode != null) {
+                CharacterDetailFragmentDirections.ToEpisodeDetailFragmentAction2 action =
+                        CharacterDetailFragmentDirections.toEpisodeDetailFragmentAction2();
+                action.setEpisodeName(clickedEpisode.getName()).setEpisodeAirDate(clickedEpisode.getAirDate())
+                        .setEpisodeCode(clickedEpisode.getCode()).setId(clickedEpisode.getId());
+                Navigation.findNavController(v).navigate(action);
+            }
     }
 }
