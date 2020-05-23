a51fc7ec6daef4640e50672457cbdde95464658f
AlexMason <shevaalex@yahoo.co.uk>
2020-05-23 19:38:44 +0100

    LocationDetailFragment redesigned

--- app/src/main/java/com/shevaalex/android/rickmortydatabase/ui/character/CharacterAdapterUtil.java
+++ app/src/main/java/com/shevaalex/android/rickmortydatabase/ui/character/CharacterAdapterUtil.java
@@ -7,7 +7,6 @@ import com.shevaalex.android.rickmortydatabase.R;
 abstract class CharacterAdapterUtil {
     private static final String VALUE_ALIVE = "Alive";
     private static final String VALUE_DEAD = "Dead";
-    private static final String VALUE_UNKNOWN = "unknown";
 
     //returns color to set TextView color depending on Character's status
     static int getStatusColour(String status, Context context) {
@@ -18,9 +17,6 @@ abstract class CharacterAdapterUtil {
                 break;
             case VALUE_DEAD:
                 color = context.getResources().getColor(R.color.rm_red_add_900);
-                break;
-            case VALUE_UNKNOWN:
-                color = context.getResources().getColor(R.color.rm_grey_900);
                 break;
         }
         return color;
