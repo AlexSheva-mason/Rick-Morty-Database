461aee34e7e6e574ca6a2fd0cca3cc075c2cf377
AlexMason <shevaalex@yahoo.co.uk>
2020-09-08 10:48:23 +0100

    OkHttpClient timeout options added

--- app/src/main/java/com/shevaalex/android/rickmortydatabase/source/network/RetrofitService.java
+++ app/src/main/java/com/shevaalex/android/rickmortydatabase/source/network/RetrofitService.java
@@ -4,6 +4,9 @@ import com.shevaalex.android.rickmortydatabase.source.network.net_utils.ApiConst
 import com.shevaalex.android.rickmortydatabase.source.network.net_utils.CallAdapterLiveDataFactory;
 import com.shevaalex.android.rickmortydatabase.source.network.requests.CharacterApi;
 
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -11,11 +14,22 @@ public class RetrofitService {
     private static final Object LOCK = new Object();
     private static volatile RetrofitService sRetrofitService;
     private CharacterApi characterApi;
+    private static final int CONNECTION_TIMEOUT = 10;
+    private static final int READ_TIMEOUT = 5;
+    private static final int WRITE_TIMEOUT = 5;
+
+    private static OkHttpClient okHttpClient = new OkHttpClient.Builder()
+            .connectTimeout(CONNECTION_TIMEOUT, TimeUnit.SECONDS)
+            .readTimeout(READ_TIMEOUT, TimeUnit.SECONDS)
+            .writeTimeout(WRITE_TIMEOUT, TimeUnit.SECONDS)
+            .retryOnConnectionFailure(false)
+            .build();
 
     private RetrofitService() {
         Retrofit.Builder builder =
                 new Retrofit.Builder()
                         .baseUrl(ApiConstants.BASE_URL)
+                        .client(okHttpClient)
                         .addCallAdapterFactory(new CallAdapterLiveDataFactory())
                         .addConverterFactory(GsonConverterFactory.create());
         Retrofit retrofit = builder.build();
