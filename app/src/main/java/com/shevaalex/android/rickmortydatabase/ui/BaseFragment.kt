47aa3aef920f67821cfc9944a12c7d4d2053ecf8
AlexMason <shevaalex@yahoo.co.uk>
2020-11-02 22:19:46 +0000

    BaseFragment kotlinised

---
+++ app/src/main/java/com/shevaalex/android/rickmortydatabase/ui/BaseFragment.kt
@@ -0,0 +1,26 @@
+package com.shevaalex.android.rickmortydatabase.ui
+
+import com.google.firebase.analytics.FirebaseAnalytics
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.analytics.ktx.logEvent
+import com.google.firebase.ktx.Firebase
+
+open class BaseFragment : Fragment() {
+
+    lateinit var firebaseAnalytics: FirebaseAnalytics
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        firebaseAnalytics = Firebase.analytics
+    }
+
+    override fun onResume() {
+        super.onResume()
+        //log screen view to firebase
+        firebaseAnalytics.logEvent(FirebaseAnalytics.Event.SCREEN_VIEW) {
+            param(FirebaseAnalytics.Param.SCREEN_CLASS, this.javaClass.simpleName)
+        }
+    }
+}
