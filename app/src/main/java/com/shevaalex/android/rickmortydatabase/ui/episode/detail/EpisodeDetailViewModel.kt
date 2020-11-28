99df95590bfffa5bdac5877e886172a7f527b7cd
AlexMason <shevaalex@yahoo.co.uk>
2020-11-28 16:34:23 +0000

    EpisodeDetailViewModel kotlinised and refactored

---
+++ app/src/main/java/com/shevaalex/android/rickmortydatabase/ui/episode/detail/EpisodeDetailViewModel.kt
@@ -0,0 +1,21 @@
+package com.shevaalex.android.rickmortydatabase.ui.episode.detail
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.switchMap
+import com.shevaalex.android.rickmortydatabase.models.character.CharacterModel
+import com.shevaalex.android.rickmortydatabase.models.episode.EpisodeModel
+import com.shevaalex.android.rickmortydatabase.repository.location.LocationDetailRepo
+import com.shevaalex.android.rickmortydatabase.ui.BaseDetailViewModel
+import javax.inject.Inject
+
+class EpisodeDetailViewModel
+@Inject
+constructor(
+        private val locationDetailRepo: LocationDetailRepo
+): BaseDetailViewModel<EpisodeModel>() {
+
+    val characters: LiveData<List<CharacterModel>> = _detailObject.switchMap {
+        locationDetailRepo.getCharacters(it.characterIds)
+    }
+
+}
