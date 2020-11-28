a5d6959f9fff99041e3b144808a22d9385e19160
AlexMason <shevaalex@yahoo.co.uk>
2020-11-28 12:26:39 +0000

    CharacterAuxAdapter refactored and kotlinised into CharacterSmallAdapter.kt

---
+++ app/src/main/java/com/shevaalex/android/rickmortydatabase/ui/CharacterSmallAdapter.kt
@@ -0,0 +1,83 @@
+package com.shevaalex.android.rickmortydatabase.ui
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.shevaalex.android.rickmortydatabase.R
+import com.shevaalex.android.rickmortydatabase.databinding.ItemCharacterSmallBinding
+import com.shevaalex.android.rickmortydatabase.models.character.CharacterModel
+import com.shevaalex.android.rickmortydatabase.utils.TextColourUtil
+
+class CharacterSmallAdapter(
+        private val characterListener: CharacterClickListener
+): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    private var characterList: List<CharacterModel>? = null
+
+    fun setCharacterList(characterList: List<CharacterModel>) {
+        this.characterList = characterList
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val itemBind = ItemCharacterSmallBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+        )
+        return CharacterSmallViewHolder(itemBind)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val character: CharacterModel? = characterList?.get(position)
+        when (holder) {
+            is CharacterSmallViewHolder -> character?.let {
+                holder.bind(
+                        character = it,
+                        characterListener = characterListener
+                )
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return characterList?.size?:0
+    }
+
+    class CharacterSmallViewHolder(
+            private val itemBind: ItemCharacterSmallBinding
+    ): RecyclerView.ViewHolder(itemBind.root) {
+
+        fun bind(character: CharacterModel, characterListener: CharacterClickListener) {
+            val context = itemBind.root.context
+            itemBind.root.setOnClickListener {
+                characterListener.onCharacterClick(character)
+            }
+            Glide.with(context)
+                    .load(character.imageUrl)
+                    .apply(RequestOptions()
+                            .placeholder(R.drawable.picasso_placeholder_error)
+                            .error(R.drawable.picasso_placeholder_error)
+                    )
+                    .into(itemBind.characterImage)
+            itemBind.characterNameValue.text = character.name
+            itemBind.characterGenderValue?.text = character.gender
+            itemBind.characterSpeciesValue?.text = character.species
+            itemBind.characterStatusValue?.text = character.status
+            if (character.status != context.resources.getString(R.string.species_unknown)) {
+                val color = TextColourUtil.getStatusColour(character.status, context)
+                itemBind.characterStatusValue?.setTextColor(color)
+            } else {
+                itemBind.characterStatusValue?.setTextColor(
+                        TextColourUtil.fetchThemeColor(R.attr.colorOnBackground, context)
+                )
+            }
+        }
+
+    }
+
+    interface CharacterClickListener {
+        fun onCharacterClick(character: CharacterModel)
+    }
+}
