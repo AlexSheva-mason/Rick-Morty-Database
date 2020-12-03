cb860fe3294f6326d8c6f6e184715c6cd40a51a8
AlexMason <shevaalex@yahoo.co.uk>
2020-12-03 19:40:52 +0000

    LocationDetailFragment navigation to CDF implemented

--- app/src/main/java/com/shevaalex/android/rickmortydatabase/ui/location/detail/LocationDetailFragment.kt
+++ app/src/main/java/com/shevaalex/android/rickmortydatabase/ui/location/detail/LocationDetailFragment.kt
@@ -5,8 +5,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -116,8 +116,8 @@ class LocationDetailFragment : BaseDetailFragment<FragmentLocationDetailBinding,
     }
 
     private fun navigateCharacterDetail(character: CharacterModel) {
-        Toast.makeText(requireContext(), character.name, Toast.LENGTH_SHORT).show()
-        // do nothing for now
+        val action = LocationDetailFragmentDirections.actionGlobalCharacterDetailFragment2(character)
+        findNavController().navigate(action)
     }
 
     private fun restoreViewState(savedInstanceState: Bundle?) {
