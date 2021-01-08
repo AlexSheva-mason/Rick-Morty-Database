0ec222a85a0a07f52e4bb85e875d7ca8c318dde3
AlexMason <shevaalex@yahoo.co.uk>
2021-01-08 15:32:07 +0000

    changed to use firebase rest api

--- app/src/main/java/com/shevaalex/android/rickmortydatabase/source/remote/CharacterApi.kt
+++ app/src/main/java/com/shevaalex/android/rickmortydatabase/source/remote/CharacterApi.kt
@@ -1,25 +1,42 @@
 package com.shevaalex.android.rickmortydatabase.source.remote
 
+import com.google.gson.JsonObject
 import com.shevaalex.android.rickmortydatabase.models.character.CharacterModel
-import com.shevaalex.android.rickmortydatabase.models.character.CharacterPageModel
-import com.shevaalex.android.rickmortydatabase.utils.networking.ApiConstants
-import com.shevaalex.android.rickmortydatabase.utils.networking.ApiResult
+import com.shevaalex.android.rickmortydatabase.utils.networking.*
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface CharacterApi {
 
-    //gets page with Characters
-    @GET(ApiConstants.ApiCallCharacterKeys.SUB_URL_CHARACTER)
-    suspend fun getCharactersPage(
-            @Query(ApiConstants.KEY_QUERY_PAGE) page: String
-    ): ApiResult<CharacterPageModel>
+    /**
+     * gets a list of all children from the character node
+     * @param idToken token to be used in Query ?auth=<idToken>
+     * @param isShallow boolean is used in Query &shallow=isShallow. Pass true to get shallow result
+     */
+    @GET(SUB_URL_CHARACTER+FIREBASE_JSON_SUFFIX)
+    suspend fun getCharacterList(
+            @Query(FIREBASE_AUTH_QUERY) idToken: String,
+            @Query(FIREBASE_SHALLOW_QUERY) isShallow: Boolean
+    ): ApiResult<JsonObject>
 
-    //gets a single Character object
-    @GET(ApiConstants.ApiCallCharacterKeys.SUB_URL_CHARACTER + "{id}")
+    /**
+     * Warning! this method will always return number of characters = n+1
+     * gets a list of all children from the character node
+     * @param idToken token to be used in Query ?auth=<idToken>
+     */
+    @GET(SUB_URL_CHARACTER+FIREBASE_JSON_SUFFIX)
+    suspend fun getCharacterList(
+            @Query(FIREBASE_AUTH_QUERY) idToken: String
+    ): ApiResult<List<CharacterModel?>>
+
+    /**
+     * gets a single character
+     */
+    @GET("$SUB_URL_CHARACTER/{id}$FIREBASE_JSON_SUFFIX")
     suspend fun getCharacter(
-            @Path("id") charId: Int
+            @Path("id") charId: Int,
+            @Query(FIREBASE_AUTH_QUERY) idToken: String
     ): ApiResult<CharacterModel>
 
 }
