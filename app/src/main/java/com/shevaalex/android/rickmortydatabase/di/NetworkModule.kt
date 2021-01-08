86dd5ad5fcdcf05ed5907d3c97ebff8d7db7540c
AlexMason <shevaalex@yahoo.co.uk>
2021-01-08 15:35:06 +0000

    okhttp logging interceptor added, retrofit base url is set depending on device language

--- app/src/main/java/com/shevaalex/android/rickmortydatabase/di/NetworkModule.kt
+++ app/src/main/java/com/shevaalex/android/rickmortydatabase/di/NetworkModule.kt
@@ -1,18 +1,18 @@
 package com.shevaalex.android.rickmortydatabase.di
 
-import com.shevaalex.android.rickmortydatabase.utils.networking.ApiConstants
 import com.shevaalex.android.rickmortydatabase.source.remote.CharacterApi
-import com.shevaalex.android.rickmortydatabase.source.remote.EpisodeApi
-import com.shevaalex.android.rickmortydatabase.source.remote.LocationApi
 import com.shevaalex.android.rickmortydatabase.utils.Constants.Companion.CONNECTION_TIMEOUT
 import com.shevaalex.android.rickmortydatabase.utils.Constants.Companion.READ_TIMEOUT
 import com.shevaalex.android.rickmortydatabase.utils.Constants.Companion.WRITE_TIMEOUT
-import com.shevaalex.android.rickmortydatabase.utils.networking.ApiResultCallAdapterFactory
+import com.shevaalex.android.rickmortydatabase.utils.networking.*
 import dagger.Module
 import dagger.Provides
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import timber.log.Timber
+import java.util.*
 import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
@@ -22,19 +22,35 @@ object NetworkModule{
     @Singleton
     @Provides
     fun provideOkHttpClient(): OkHttpClient {
+        val interceptor = HttpLoggingInterceptor { message ->
+            Timber.d(message)
+        }.setLevel(HttpLoggingInterceptor.Level.BASIC)
         return OkHttpClient.Builder()
                 .connectTimeout(CONNECTION_TIMEOUT, TimeUnit.SECONDS)
                 .readTimeout(READ_TIMEOUT, TimeUnit.SECONDS)
                 .writeTimeout(WRITE_TIMEOUT, TimeUnit.SECONDS)
                 .retryOnConnectionFailure(false)
+                .addInterceptor(interceptor)
                 .build()
     }
+
 
     @Singleton
     @Provides
     fun provideRetrofitBuilder(okHttpClient: OkHttpClient): Retrofit.Builder{
+        val baseUrl = when {
+            Locale.getDefault().language.startsWith(LOCALE_PREFIX_RU) -> {
+                BASE_URL_RU
+            }
+            Locale.getDefault().language.startsWith(LOCALE_PREFIX_UK) -> {
+                BASE_URL_UK
+            }
+            else -> {
+                BASE_URL_EN
+            }
+        }
         return Retrofit.Builder()
-                .baseUrl(ApiConstants.BASE_URL)
+                .baseUrl(baseUrl)
                 .client(okHttpClient)
                 .addCallAdapterFactory(ApiResultCallAdapterFactory())
                 .addConverterFactory(GsonConverterFactory.create())
@@ -46,22 +62,6 @@ object NetworkModule{
         return retrofitBuilder
                 .build()
                 .create(CharacterApi::class.java)
-    }
-
-    @Singleton
-    @Provides
-    fun provideLocationApiService(retrofitBuilder: Retrofit.Builder): LocationApi {
-        return retrofitBuilder
-                .build()
-                .create(LocationApi::class.java)
-    }
-
-    @Singleton
-    @Provides
-    fun provideEpisodeApiService(retrofitBuilder: Retrofit.Builder): EpisodeApi {
-        return retrofitBuilder
-                .build()
-                .create(EpisodeApi::class.java)
     }
 
 }
