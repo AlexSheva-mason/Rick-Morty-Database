94ff8809332422203c181cfda110ca52550b03b1
AlexMason <shevaalex@yahoo.co.uk>
2021-01-08 15:48:31 +0000

    file kotlinised, redundant values deleted

---
+++ app/src/main/java/com/shevaalex/android/rickmortydatabase/utils/networking/ApiConstants.kt
@@ -0,0 +1,16 @@
+package com.shevaalex.android.rickmortydatabase.utils.networking
+
+const val FIREBASE_JSON_SUFFIX = ".json"
+const val LOCALE_PREFIX_RU = "ru"
+const val LOCALE_PREFIX_UK = "uk"
+const val BASE_URL_EN = "https://rick-and-morty-database.firebaseio.com/en/"
+const val BASE_URL_RU = "https://rick-and-morty-database.firebaseio.com/ru/"
+const val BASE_URL_UK = "https://rick-and-morty-database.firebaseio.com/uk/"
+const val SUB_URL_CHARACTER = "character"
+const val SUB_URL_LOCATION = "location"
+const val SUB_URL_EPISODE = "episode"
+const val FIREBASE_SHALLOW_QUERY = "shallow"
+const val FIREBASE_AUTH_QUERY = "auth"
+const val LOCATION_RESIDENTS = "residents"
+const val EPISODE_CODE = "episode"
+const val EPISODE_CHARACTERS = "characters"
