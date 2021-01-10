e44a0290288c84c783e0eec8f27fe8c6e9b17976
AlexMason <shevaalex@yahoo.co.uk>
2021-01-10 17:23:25 +0000

    currentTimeMinutes() util function added

--- app/src/main/java/com/shevaalex/android/rickmortydatabase/utils/UtilFunctions.kt
+++ app/src/main/java/com/shevaalex/android/rickmortydatabase/utils/UtilFunctions.kt
@@ -3,3 +3,7 @@ package com.shevaalex.android.rickmortydatabase.utils
 fun currentTimeHours(): Long {
     return System.currentTimeMillis()/3600000
 }
+
+fun currentTimeMinutes(): Long {
+    return System.currentTimeMillis()/60000
+}
