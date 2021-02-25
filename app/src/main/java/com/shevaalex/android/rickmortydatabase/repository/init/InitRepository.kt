729a94dbff0e8d2fa3bf9c085108252bc2279401
AlexMason <shevaalex@yahoo.co.uk>
2021-02-25 10:31:55 +0000

    InitRepository refactored to interface and implementation

--- app/src/main/java/com/shevaalex/android/rickmortydatabase/repository/init/InitRepository.kt
+++ app/src/main/java/com/shevaalex/android/rickmortydatabase/repository/init/InitRepository.kt
@@ -1,65 +1,10 @@
 package com.shevaalex.android.rickmortydatabase.repository.init
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.liveData
 import com.shevaalex.android.rickmortydatabase.utils.networking.StateResource
-import com.shevaalex.android.rickmortydatabase.utils.networking.Status
-import timber.log.Timber
-import javax.inject.Inject
-import javax.inject.Singleton
 
-@Singleton
-class InitRepository
-@Inject
-constructor(
-        private val characterInit: CharacterInitManagerImpl,
-        private val locationInit: LocationInitManagerImpl,
-        private val episodeInit: EpisodeInitManagerImpl
-) {
+interface InitRepository {
 
-    fun getDbStateResource(token: String): LiveData<StateResource> = liveData {
-        emit(StateResource(Status.Loading))
-        val initFuns = listOf(
-                initCharacters(token),
-                initLocations(token),
-                initEpisodes(token))
-        Timber.i("init funs results: %s, %s, %s",
-                initFuns[0].status.toString(),
-                initFuns[1].status.toString(),
-                initFuns[2].status.toString())
-        //if there is at least one error whilst initiating db -> emit Status.Error
-        initFuns.find { errorStateResource: StateResource ->
-            errorStateResource.status == Status.Error
-        }?.let {
-            emit(it)
-        }
-        //if all results are success -> emit Status.Success
-        val successStatus = { successStateResource: StateResource ->
-            successStateResource.status == Status.Success
-        }
-        if (initFuns.all(successStatus)) {
-            initFuns.find(successStatus)?.let {
-                emit(it)
-            }
-        }
-    }
-
-    /**
-     * Initialises and syncs Character table in the database
-     */
-    private suspend fun initCharacters(token: String) =
-            characterInit.initTable(token, "Character")
-
-    /**
-     * Initialises and syncs Location table in the database
-     */
-    private suspend fun initLocations(token: String) =
-            locationInit.initTable(token, "Location")
-
-    /**
-     * Initialises and syncs Episode table in the database
-     */
-    private suspend fun initEpisodes(token: String) =
-            episodeInit.initTable(token, "Episode")
+    fun getDbStateResource(token: String): LiveData<StateResource>
 
 }
---
+++ app/src/main/java/com/shevaalex/android/rickmortydatabase/repository/init/InitRepositoryImpl.kt
@@ -0,0 +1,68 @@
+package com.shevaalex.android.rickmortydatabase.repository.init
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.liveData
+import com.shevaalex.android.rickmortydatabase.models.character.CharacterModel
+import com.shevaalex.android.rickmortydatabase.models.episode.EpisodeModel
+import com.shevaalex.android.rickmortydatabase.models.location.LocationModel
+import com.shevaalex.android.rickmortydatabase.utils.networking.StateResource
+import com.shevaalex.android.rickmortydatabase.utils.networking.Status
+import timber.log.Timber
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class InitRepositoryImpl
+@Inject
+constructor(
+        private val characterInit: InitManager<CharacterModel>,
+        private val locationInit: InitManager<LocationModel>,
+        private val episodeInit: InitManager<EpisodeModel>
+): InitRepository {
+
+    override fun getDbStateResource(token: String): LiveData<StateResource> = liveData {
+        emit(StateResource(Status.Loading))
+        val initFuns = listOf(
+                initCharacters(token),
+                initLocations(token),
+                initEpisodes(token))
+        Timber.i("init funs results: %s, %s, %s",
+                initFuns[0].status.toString(),
+                initFuns[1].status.toString(),
+                initFuns[2].status.toString())
+        //if there is at least one error whilst initiating db -> emit Status.Error
+        initFuns.find { errorStateResource: StateResource ->
+            errorStateResource.status == Status.Error
+        }?.let {
+            emit(it)
+        }
+        //if all results are success -> emit Status.Success
+        val successStatus = { successStateResource: StateResource ->
+            successStateResource.status == Status.Success
+        }
+        if (initFuns.all(successStatus)) {
+            initFuns.find(successStatus)?.let {
+                emit(it)
+            }
+        }
+    }
+
+    /**
+     * Initialises and syncs Character table in the database
+     */
+    private suspend fun initCharacters(token: String) =
+            characterInit.initTable(token, "Character")
+
+    /**
+     * Initialises and syncs Location table in the database
+     */
+    private suspend fun initLocations(token: String) =
+            locationInit.initTable(token, "Location")
+
+    /**
+     * Initialises and syncs Episode table in the database
+     */
+    private suspend fun initEpisodes(token: String) =
+            episodeInit.initTable(token, "Episode")
+
+}
