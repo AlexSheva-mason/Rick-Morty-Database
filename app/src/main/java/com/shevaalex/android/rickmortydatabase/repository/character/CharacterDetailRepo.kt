9c05cc7dfa952254f554916234ace3605c37da74
AlexMason <shevaalex@yahoo.co.uk>
2021-02-27 13:36:44 +0000

    CharacterDetailRepo refactored to interface and implementation

--- app/src/main/java/com/shevaalex/android/rickmortydatabase/repository/character/CharacterDetailRepo.kt
+++ app/src/main/java/com/shevaalex/android/rickmortydatabase/repository/character/CharacterDetailRepo.kt
@@ -3,22 +3,11 @@ package com.shevaalex.android.rickmortydatabase.repository.character
 import androidx.lifecycle.LiveData
 import com.shevaalex.android.rickmortydatabase.models.episode.EpisodeModel
 import com.shevaalex.android.rickmortydatabase.models.location.LocationModel
-import com.shevaalex.android.rickmortydatabase.source.local.EpisodeModelDao
-import com.shevaalex.android.rickmortydatabase.source.local.LocationModelDao
-import javax.inject.Inject
-import javax.inject.Singleton
 
-@Singleton
-class CharacterDetailRepo
-@Inject
-constructor(
-        private val episodeDao: EpisodeModelDao,
-        private val locationDao: LocationModelDao
-) {
+interface CharacterDetailRepo {
 
-    fun getLocationById(id: Int): LiveData<LocationModel> = locationDao.getLocationById(id)
+    fun getLocationById(id: Int): LiveData<LocationModel>
 
-    fun getEpisodes(episodeIds: List<Int>): LiveData<List<EpisodeModel>> =
-            episodeDao.getEpisodesByIds(episodeIds)
+    fun getEpisodes(episodeIds: List<Int>): LiveData<List<EpisodeModel>>
 
 }
