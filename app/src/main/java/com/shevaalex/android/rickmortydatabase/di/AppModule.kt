b889d199aae64b86b86b95e78a1b7b480b862fc1
AlexMason <shevaalex@yahoo.co.uk>
2021-02-28 15:01:04 +0000

    repositories interface dependency declarations refactored

--- app/src/main/java/com/shevaalex/android/rickmortydatabase/di/AppModule.kt
+++ app/src/main/java/com/shevaalex/android/rickmortydatabase/di/AppModule.kt
@@ -6,16 +6,12 @@ import android.content.SharedPreferences
 import com.google.android.play.core.review.ReviewManager
 import com.google.android.play.core.review.ReviewManagerFactory
 import com.google.android.play.core.review.testing.FakeReviewManager
-import com.shevaalex.android.rickmortydatabase.models.character.CharacterModel
-import com.shevaalex.android.rickmortydatabase.models.episode.EpisodeModel
-import com.shevaalex.android.rickmortydatabase.models.location.LocationModel
-import com.shevaalex.android.rickmortydatabase.repository.init.*
 import com.shevaalex.android.rickmortydatabase.utils.Constants.Companion.SHARED_PREFS_FILE_NAME
 import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
 
-@Module
+@Module(includes = [RepositoryDeclarations::class])
 object AppModule {
 
     @Singleton
@@ -35,21 +31,5 @@ object AppModule {
     fun provideSharedPreferences(app: Application): SharedPreferences {
         return app.getSharedPreferences(SHARED_PREFS_FILE_NAME, Context.MODE_PRIVATE)
     }
-
-    @Singleton
-    @Provides
-    fun provideCharacterInitManager(cim: CharacterInitManagerImpl): InitManager<CharacterModel> = cim
-
-    @Singleton
-    @Provides
-    fun provideLocationInitManager(lim: LocationInitManagerImpl): InitManager<LocationModel> = lim
-
-    @Singleton
-    @Provides
-    fun provideEpisodeInitManager(eim: EpisodeInitManagerImpl): InitManager<EpisodeModel> = eim
-
-    @Singleton
-    @Provides
-    fun provideInitRepository(iri: InitRepositoryImpl): InitRepository = iri
 
 }
---
+++ app/src/main/java/com/shevaalex/android/rickmortydatabase/di/RepositoryDeclarations.kt
@@ -0,0 +1,30 @@
+package com.shevaalex.android.rickmortydatabase.di
+
+import com.shevaalex.android.rickmortydatabase.models.character.CharacterModel
+import com.shevaalex.android.rickmortydatabase.models.episode.EpisodeModel
+import com.shevaalex.android.rickmortydatabase.models.location.LocationModel
+import com.shevaalex.android.rickmortydatabase.repository.character.CharacterDetailRepo
+import com.shevaalex.android.rickmortydatabase.repository.character.CharacterDetailRepoImpl
+import com.shevaalex.android.rickmortydatabase.repository.init.*
+import dagger.Binds
+import dagger.Module
+
+@Module
+interface RepositoryDeclarations {
+
+    @Binds
+    fun bindCharacterInitManager(cim: CharacterInitManagerImpl): InitManager<CharacterModel>
+
+    @Binds
+    fun bindLocationInitManager(lim: LocationInitManagerImpl): InitManager<LocationModel>
+
+    @Binds
+    fun bindEpisodeInitManager(eim: EpisodeInitManagerImpl): InitManager<EpisodeModel>
+
+    @Binds
+    fun bindInitRepository(iri: InitRepositoryImpl): InitRepository
+
+    @Binds
+    fun bindCharacterDetailRepo(cdr: CharacterDetailRepoImpl): CharacterDetailRepo
+
+}
