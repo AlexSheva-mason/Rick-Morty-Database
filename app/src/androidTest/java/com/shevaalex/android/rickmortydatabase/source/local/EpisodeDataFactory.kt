64e4b0be6ee58ec7929254a797c2b0c3d908fcc2
AlexMason <shevaalex@yahoo.co.uk>
2021-03-02 13:12:33 +0000

    EpisodeDaoTest flaky tests fix

--- app/src/androidTest/java/com/shevaalex/android/rickmortydatabase/source/local/EpisodeDataFactory.kt
+++ app/src/androidTest/java/com/shevaalex/android/rickmortydatabase/source/local/EpisodeDataFactory.kt
@@ -1,6 +1,6 @@
 package com.shevaalex.android.rickmortydatabase.source.local
 
-import com.shevaalex.android.rickmortydatabase.models.episode.EpisodeModel
+import com.shevaalex.android.rickmortydatabase.models.episode.EpisodeEntity
 import com.shevaalex.android.rickmortydatabase.utils.Constants.Companion.VALUE_MAP_FILTER_EPISODE_S_01
 import com.shevaalex.android.rickmortydatabase.utils.Constants.Companion.VALUE_MAP_FILTER_EPISODE_S_02
 import com.shevaalex.android.rickmortydatabase.utils.Constants.Companion.VALUE_MAP_FILTER_EPISODE_S_03
@@ -10,7 +10,7 @@ import javax.inject.Singleton
 
 @Singleton
 class EpisodeDataFactory
-@Inject constructor(): DataFactory<EpisodeModel>() {
+@Inject constructor() : DataFactory<EpisodeEntity>() {
 
     private val episodeSeasonList = listOf(
             VALUE_MAP_FILTER_EPISODE_S_01,
@@ -19,13 +19,13 @@ class EpisodeDataFactory
             VALUE_MAP_FILTER_EPISODE_S_04
     )
 
-    override fun produceObjectModel(id: Int): EpisodeModel {
-        return EpisodeModel(
+    override fun produceObjectModel(id: Int): EpisodeEntity {
+        return EpisodeEntity(
                 id = id,
                 name = "testName$id",
                 imageUrl = "testImageUrl$id",
                 airDate = "testAirDate$id",
-                code = "${episodeSeasonList.random()}E99",
+                code = "${episodeSeasonList[id % episodeSeasonList.size]}E99",
                 description = "testDescription$id",
                 charactersList = listOf()
         )
