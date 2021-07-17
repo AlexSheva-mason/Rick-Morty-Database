731c99b120c7eff05de5f48a1e595a5f7467fa45
AlexMason <shevaalex@yahoo.co.uk>
2021-07-17 14:33:00 +0100

    adds firebase exception logging functionality

--- app/src/main/java/com/shevaalex/android/rickmortydatabase/utils/firebase/FirebaseLogger.kt
+++ app/src/main/java/com/shevaalex/android/rickmortydatabase/utils/firebase/FirebaseLogger.kt
@@ -3,9 +3,13 @@ package com.shevaalex.android.rickmortydatabase.utils.firebase
 interface FirebaseLogger {
 
     fun logFirebaseEvent(
-            eventName: String,
-            paramKey: String,
-            paramValue: String
+        eventName: String,
+        paramKey: String,
+        paramValue: String
+    )
+
+    fun logException(
+        exception: Exception
     )
 
 }
--- app/src/main/java/com/shevaalex/android/rickmortydatabase/utils/firebase/FirebaseLoggerImpl.kt
+++ app/src/main/java/com/shevaalex/android/rickmortydatabase/utils/firebase/FirebaseLoggerImpl.kt
@@ -2,24 +2,32 @@ package com.shevaalex.android.rickmortydatabase.utils.firebase
 
 import com.google.firebase.analytics.ktx.analytics
 import com.google.firebase.analytics.ktx.logEvent
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import com.google.firebase.ktx.Firebase
+import java.lang.Exception
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class FirebaseLoggerImpl
 @Inject
-constructor(): FirebaseLogger {
+constructor() : FirebaseLogger {
 
     private val firebaseAnalytics = Firebase.analytics
+    private val firebaseCrashlytics = FirebaseCrashlytics.getInstance()
 
     override fun logFirebaseEvent(
-            eventName: String,
-            paramKey: String,
-            paramValue: String
+        eventName: String,
+        paramKey: String,
+        paramValue: String
     ) {
         firebaseAnalytics.logEvent(eventName) {
             param(paramKey, paramValue)
         }
     }
+
+    override fun logException(exception: Exception) {
+        firebaseCrashlytics.recordException(exception)
+    }
+
 }
